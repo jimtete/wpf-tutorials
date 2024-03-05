ecf3aae6a55ba401d0679d4a0368edca499616d3
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2024-03-05 09:53:24 +0200

    fixing changes

--- WPFTutorial_01/WPFTutorial/WPFTutorial/obj/Debug/net8.0-windows/WPFTutorial_eoyu4jaa_wpftmp.AssemblyInfo.cs
+++ WPFTutorial_01/WPFTutorial/WPFTutorial/obj/Debug/net8.0-windows/WPFTutorial_eoyu4jaa_wpftmp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+419dda3fe7ff6325ebaf61e224f8cfba3380cd7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

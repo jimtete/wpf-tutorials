2158329d31b94f28517ebe74579df14698574a0f
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2024-03-06 11:55:52 +0200

    Implementation

--- WPFTutorial_01/WPFTutorial/WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial_01/WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -11,34 +11,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        lvEntries.Items.Add("a");
-        lvEntries.Items.Add("b");
-        lvEntries.Items.Add("c");
-    }
-
-    private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
-    {
-        lvEntries.Items.Add(txtEntry.Text);
-        txtEntry.Clear();
-    }
-
-    private void BtnDelete_OnClick(object sender, RoutedEventArgs e)
-    {
-        var items = lvEntries.SelectedItems;
-        
-        var result = MessageBox.Show($"Are you sure you want to delete {items.Count} items.", "Sure?", MessageBoxButton.YesNo);
-        if (result == MessageBoxResult.Yes)
-        {
-            var itemsList = new ArrayList(items);
-            foreach (var item in itemsList)
-            {
-                lvEntries.Items.Remove(item);
-            }
-        }
-    }
-
-    private void BtnClear_OnClick(object sender, RoutedEventArgs e)
-    {
-        lvEntries.Items.Clear();
     }
 }
